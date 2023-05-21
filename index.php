778f3d29c553904449f5e14eda11cb22c894ef64
AndreaMessene <amessene97@gmail.com>
2023-05-21 18:22:08 +0200

    creazione tabella con stampa delle informazioni

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!-- Stampare tutti i nostri hotel con tutti i dati disponibili.
 Iniziate in modo graduale.
+
 Prima stampate in pagina i dati, senza preoccuparvi dello stile.
 Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
 Bonus:
@@ -12,7 +13,7 @@ Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gl
 
 
 
-<!-- arrey con nomi e varie info di hotel -->
+<!-- arrey associativo multidimensionale -->
 <?php
 
     $hotels = [
@@ -55,8 +56,6 @@ Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gl
 
     ];
 
-    var_dump($hotels)
-
 ?>
 
 <!---------------------  ----------------------->
@@ -65,4 +64,70 @@ Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gl
 <?php
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+</head>
+<body>
+
+
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome Hotel</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td><?php echo ($hotels[0]['name']) ?></td>
+                    <td><?php echo ($hotels[0]['vote']) ?></td>
+                    <td><?php echo ($hotels[0]['distance_to_center']) ?></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td><?php echo ($hotels[1]['name']) ?></td>
+                    <td><?php echo ($hotels[1]['vote']) ?></td>
+                    <td><?php echo ($hotels[1]['distance_to_center']) ?> km</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td> <?php echo ($hotels[2]['name']) ?></td>
+                    <td> <?php echo ($hotels[2]['vote']) ?></td>
+                    <td> <?php echo ($hotels[2]['distance_to_center']) ?> km</td>
+                </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td> <?php echo ($hotels[3]['name']) ?></td>
+                    <td> <?php echo ($hotels[3]['vote']) ?></td>
+                    <td> <?php echo ($hotels[3]['distance_to_center']) ?> km</td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td> <?php echo ($hotels[4]['name']) ?></td>
+                    <td> <?php echo ($hotels[4]['vote']) ?></td>
+                    <td> <?php echo ($hotels[4]['distance_to_center']) ?> km</td>
+                </tr>
+            </tbody>
+        </table>    
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
 
